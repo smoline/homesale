a08dcebcc1344a72323dbd93da07e99f8e761ad1
Sherilyn Moline <smoline@gmail.com>
2017-04-29 15:53:02 -0400

    Image Modals working, no more worries about asset pipeline issues

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -4,7 +4,9 @@
 $(document).ready(function() {
 
   $('#query').on('input', function(event) {
-    var queryValue = $(this).val()
+    let queryValue = $(this).val()
+
+    console.log(`You are searching for ${queryValue}`)
 
     // $('#spinner').show()
 
